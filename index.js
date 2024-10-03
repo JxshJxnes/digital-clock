2c3bb26608d612f1728cc57a2b49284bdf886c66
JxshJxnes <joshjones1896@gmail.com>
2024-10-03 15:13:33 +0100

    AM/PM alternative
    An AM/PM alternative to 24hr

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function updateClock(){
 
-const now = new Date();
-const hours = now.getHours().toString().padStart(2, 0);
-const minutes = now.getMinutes().toString().padStart(2, 0);
-const seconds = now.getSeconds().toString().padStart(2, 0);
-const timeString = `${hours}:${minutes}:${seconds}`;
+    const now = new Date();
+    let hours = now.getHours();
+        const meridiem = hours >= 12 ? "PM" : "AM";
+        hours = hours % 12 || 12;
+        hours = hours.toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
 
-document.getElementById("clock").textContent = timeString;
+    document.getElementById("clock").textContent = timeString;
 }
 
 updateClock();
